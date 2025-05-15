94e3973ef7f829a7ae9b3799fab2e76f5820792a
LuminovaTrustCaptial <atlanticscapital@gmail.com>
2025-05-15 12:05:19 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3054196920"
+    serviceWorkerVersion: "2196824390"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -101,14 +101,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0f2e459c80bff5e335d179baab23ae02
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "674f48f45dcd2fc51063cd70d5271169",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "403e15e44dca22269203eafc05c4dad1",
+"flutter_bootstrap.js": "b22ad339f2b9be6daadf9073cbf60015",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "3a26bf9c5780041616c30ad269b478c3",
 "/": "3a26bf9c5780041616c30ad269b478c3",
-"main.dart.js": "86ed92890ebf5a6430905e0f8a67155a",
+"main.dart.js": "6753087e8fa45bad3bd3f88629c9da09",
 "manifest.json": "ef460d5726af32919a4c346bac478c49",
 "version.json": "35f3a951f4e3e458b04f0c7bbe2c6600"};
 // The application shell files that are downloaded before a service worker can
