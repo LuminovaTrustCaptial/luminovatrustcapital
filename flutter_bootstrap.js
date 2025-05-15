3225f93f66c9ca72d4a5e597e2685ff5e30dbe70
LuminovaTrustCaptial <atlanticscapital@gmail.com>
2025-05-15 13:07:55 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2196824390"
+    serviceWorkerVersion: "1297774377"
   }
 });
